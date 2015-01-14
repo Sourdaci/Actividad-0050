8d8988d51f1204f8e28f2580b92ceb75e111294d
Sourdaci <sourdaci@gmail.com>
2015-01-14 16:49:52 +0100

    metodo para multiplicar 2 numeros

--- Test0050.java
+++ Test0050.java
@@ -1,11 +1,32 @@
 
 /**
  * Actividad 0050
+ * Por exigencias de realizacion, solo esta permitido operar con sumas y restas
+ * Los resultados devueltos son numeros enteros
  */
 public class Test0050{
     /**
      * Constructor for objects of class Test0050
      */
     public Test0050(){
+    }
+    
+    /**
+     * Multiplica 2 numeros enteros
+     */
+    public int multiNum(int base, int aumento){
+        int resultado = 0;
+        if(base != 0){
+            if(aumento > 0){
+                for (int contador = 1;contador <= aumento; contador++){
+                    resultado += base;
+                }
+            }else if(aumento < 0){
+                for (int contador = -1;contador >= aumento; contador--){
+                    resultado += -base;
+                }
+            }
+        }
+        return resultado;
     }
 }
