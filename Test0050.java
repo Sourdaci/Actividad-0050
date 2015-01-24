394a18ef214dbd19b49b166577020140e488d7c3
Sourdaci <sourdaci@gmail.com>
2015-01-24 02:02:45 +0100

    Fuera de plazo: metodos de multiplicacion y division comentados

--- Test0050.java
+++ Test0050.java
@@ -8,24 +8,35 @@ public class Test0050{
     /**
      * Constructor for objects of class Test0050
      */
+    
+    // no se necesita almacenar ninguna informacion durante la ejecucion del programa
+    
     public Test0050(){
+        // Todo se hace con parametros y variables locales, no hay atributos para inicializar
     }
     
     /**
      * Multiplica 2 numeros enteros
      */
     public int multiNum(int base, int aumento){
-        int resultado = 0;
-        if(base != 0){
-            if(aumento > 0){
+        int resultado = 0; // resultado por defecto si algun operando es cero
+        if(base != 0){// cero por algo es cero, descartamos la base cero para evitar calculos
+            if(aumento > 0){// si el factor multiplicador es positivo
+                // el bucle va avanzando hacia delante hasta que el contador alcance al aumento
                 for (int contador = 1; contador <= aumento; contador++){
+                    // sumamos la base al resultado
                     resultado += base;
                 }
-            }else if(aumento < 0){
+                // con base positiva el resultado sera positivo, con negativa sera negativo
+            }else if(aumento < 0){// si el factor multiplicador es negativo
+                // el bucle va avanzando hacia atras hasta que el contador alcance al aumento
                 for (int contador = -1; contador >= aumento; contador--){
+                    // sumamos la base con signo cambiado al resultado
                     resultado += -base;
                 }
+                // con base positiva el resultado sera negativo, con negativa sera positivo
             }
+            // algo por cero es indeterminacion, si se da el caso se escoge el resultado cero
         }
         return resultado;
     }
@@ -34,32 +45,40 @@ public class Test0050{
      * Divide 2 numeros enteros
      */
     public int diviNum(int dividendo, int divisor){
-        int contador = 0;
-        if(dividendo > 0){
-            int auxiliar = dividendo;
-            if(divisor > 0){
+        int contador = 0; // resultado por defecto si algun operando es cero
+        if(dividendo > 0){ // dividendo positivo
+            int auxiliar = dividendo; // guardamos el dividendo
+            if(divisor > 0){ // division de numeros positivos
+                // se restara el divisor hasta que el auxiliar sea menor
                 while(auxiliar >= divisor){
                     auxiliar -= divisor;
-                    contador++;
+                    contador++; // incremento
                 }
-            }else if(divisor < 0){
-                while(auxiliar >= -divisor){
+                // resultado positivo
+            }else if(divisor < 0){ // divisor negativo
+                // se resta el positivo del divisor al auxiliar
+                while(auxiliar >= -divisor){ // se compara el inverso del divisor: positivo
                     auxiliar -= -divisor;
-                    contador--;
+                    contador--; // decremento
                 }
+                // resultado negativo
             }
-        }else if(dividendo < 0){
-            int auxiliar = -dividendo;
-            if(divisor > 0){
+        }else if(dividendo < 0){ // dividendo negativo
+            int auxiliar = -dividendo; // guardamos el inverso del dividendo
+            if(divisor > 0){ // divisor positivo
+                // se restara el divisor hasta que el auxiliar sea menor
                 while(auxiliar >= divisor){
                     auxiliar -= divisor;
-                    contador--;
+                    contador--; // decremento
                 }
-            }else if(divisor < 0){
-                while(auxiliar >= -divisor){
+                // resultado negativo
+            }else if(divisor < 0){ // division de numeros negativos
+                // se resta el positivo del divisor al auxiliar
+                while(auxiliar >= -divisor){ // se compara el inverso del divisor: positivo
                     auxiliar -= -divisor;
-                    contador++;
+                    contador++; // incremento
                 }
+                // resultado positivo
             }
         }
         return contador;
@@ -69,36 +88,30 @@ public class Test0050{
      * Divide 2 numeros enteros y calcula el resto
      */
     public int resto(int dividendo, int divisor){
-        int contador = 0;
         int auxiliar = 0;
         if(dividendo > 0){
             auxiliar = dividendo;
             if(divisor > 0){
                 while(auxiliar >= divisor){
                     auxiliar -= divisor;
-                    contador++;
                 }
             }else if(divisor < 0){
                 while(auxiliar >= -divisor){
                     auxiliar -= -divisor;
-                    contador--;
                 }
-                auxiliar = -auxiliar;
             }
         }else if(dividendo < 0){
             auxiliar = -dividendo;
             if(divisor > 0){
                 while(auxiliar >= divisor){
                     auxiliar -= divisor;
-                    contador--;
                 }
-                auxiliar = -auxiliar;
             }else if(divisor < 0){
                 while(auxiliar >= -divisor){
                     auxiliar -= -divisor;
-                    contador++;
                 }
             }
+            auxiliar = -auxiliar;
         }
         return auxiliar;
     }
